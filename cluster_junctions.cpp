c7619e980b5a03863e531a72f2eae9af1eff26ac
Zachery Dickson <dicksoz@mcmaster.ca>
2024-11-20 21:14:02 +0200

    Reimplemented cluster_junctions to increase processivity

--- cluster_junctions.cpp
+++ cluster_junctions.cpp
@@ -53,10 +53,21 @@ struct junctionRegion_t {
 
 
 typedef std::vector<jRegLabel_t> jRegLabelVector_t;
-typedef std::unordered_map<jRegLabel_t,size_t,jRegLabel_HashFunctor,jRegLabel_EqFunctor>
+typedef std::unordered_map< jRegLabel_t,size_t,jRegLabel_HashFunctor,
+			    jRegLabel_EqFunctor>
 	    jRegLabelCount_t;
-typedef std::unordered_map<jRegLabel_t,junctionRegion_t,jRegLabel_HashFunctor,jRegLabel_EqFunctor>
+typedef std::unordered_map< jRegLabel_t,bool,jRegLabel_HashFunctor,
+			    jRegLabel_EqFunctor>
+	    jRegSplitStatus_t;
+typedef std::unordered_map< jRegLabel_t,junctionRegion_t,jRegLabel_HashFunctor,
+			    jRegLabel_EqFunctor>
 	    jRegMap_t;
+typedef std::unordered_set< jRegLabel_t, jRegLabel_HashFunctor,
+			    jRegLabel_EqFunctor>
+	    jRegLabelSet_t;
+typedef std::unordered_map< jRegLabel_t, jRegLabelSet_t,
+			    jRegLabel_HashFunctor,jRegLabel_EqFunctor> 
+	    BestJRegSetMap_t;
 
 //==== GLOBAL VARIABLE DECLARATIONS
 
@@ -70,8 +81,9 @@ std::unordered_set<std::string> VirusNameSet;
 
 void OrderJunctions(const std::string fname, jRegLabelCount_t & labelCount,
 		    jRegLabelVector_t & labelVec);
-void ClusterRegions(const std::string fname, const jRegLabelCount_t & labelCount,
-		    const jRegLabelVector_t & labelVec, jRegMap_t & regionMap);
+void IdentifyBestJunctions( const std::string fname,BestJRegSetMap_t & bestSetMap);
+void ClusterRegions(const std::string fname, const BestJRegSetMap_t & bestSetMap,
+		    jRegMap_t & regionMap);
 void FilterRegions(jRegMap_t & regionMap);
 void OutputRegions(std::string fname, const jRegMap_t & regionMap);
 
@@ -103,9 +115,10 @@ int main(int argc, char* argv[]) {
     jRegLabelVector_t labelVec;
     jRegLabelCount_t labelCount;
 
-    OrderJunctions(candidate_file_name,labelCount,labelVec);
+    BestJRegSetMap_t regionAssignments;
+    IdentifyBestJunctions(candidate_file_name,regionAssignments);
     jRegMap_t regionMap;
-    ClusterRegions(candidate_file_name,labelCount,labelVec,regionMap);
+    ClusterRegions(candidate_file_name,regionAssignments,regionMap);
     FilterRegions(regionMap);
     OutputRegions(reg_file_name,regionMap);
     fprintf(stderr,"Done - cluster_junctions\n");
@@ -115,12 +128,16 @@ int main(int argc, char* argv[]) {
 
 //Parsed the candidate junctions and counts the instances of each junction
 //Then outputs a vector of junction labels ordered from most to least numerous
+//As a side effect the split status for the labels is recorded
 //Inputs - a string representing the file containing candidate junctions
 //	 - a reference to a mapping of counts for labels
 //	 - a reference to a vector of labels
-//Output - None, modifies the label count map and vector
+//	 - a reference to a mapping of split status for labels
+//Output - None, modifies the label count map and vector and split status map
 void OrderJunctions(const std::string fname, jRegLabelCount_t & labelCount,
-		    jRegLabelVector_t & labelVec) {
+		    jRegLabelVector_t & labelVec,
+		    jRegSplitStatus_t & labelFromSplitOnly)
+{
     fprintf(stderr,"Ordering Junctions ...\n");
     std::ifstream in(fname);
     std::string chr,qname;
@@ -129,11 +146,14 @@ void OrderJunctions(const std::string fname, jRegLabelCount_t & labelCount,
     ;
     while (in >> chr >> off >> end >> qname >> score >> strand){
 	jRegLabel_t label = {chr,strand,off};
+	bool bSplit = (qname[qname.length()-2] == '_');
 	if(!labelCount.count(label)) {
 	    labelCount[label] = 0;
 	    labelVec.push_back(label);
+	    labelFromSplitOnly[label] = true;
 	}
 	labelCount[label]++;
+	if(!bSplit) labelFromSplitOnly[label] = false;
     }
     //Sort the vector of labels in descending order by count
     //sort's comparator is true if less (i.e earlier in sorted order),
@@ -146,6 +166,54 @@ void OrderJunctions(const std::string fname, jRegLabelCount_t & labelCount,
     fprintf(stderr,"Ordered %lu unique Junctions\n",labelVec.size());
 }
 
+//First loads all unique junctions and sorts them by order of prevalance
+//Then assigns each junction to the best junction within range
+//Inputs - a string representing the file containing candidate junctions
+//	 - a reference to a map from label to best label set
+//Output - None, modifies the best set map
+void IdentifyBestJunctions( const std::string fname,
+			    BestJRegSetMap_t & bestSetMap) {
+    jRegLabelVector_t labelVec;
+    jRegLabelCount_t labelCount;
+    jRegSplitStatus_t labelFromSplitOnly;
+    OrderJunctions(fname,labelCount,labelVec,labelFromSplitOnly);
+
+    //Iterate over junctions
+    for(size_t i = 0; i < labelVec.size(); ){
+	size_t first = i; 
+	do {
+	    jRegLabel_t & aLabel = labelVec[i];
+	    size_t aRange = (labelFromSplitOnly[aLabel]) ?	ReadLength : 
+								MaxInsertSize;
+	    size_t left = (aLabel.pos > aRange) ? aLabel.pos - aRange : 0;
+	    size_t right = aLabel.pos + aRange;
+	    for(size_t pos = left; pos <= right; pos++){
+		jRegLabel_t bLabel = {aLabel.chr, aLabel.strand, pos};
+		//Check if the potential junction exists
+		if(!labelCount.count(bLabel)) continue;
+		//Make sure the other junction is mutually in range
+		size_t bRange = (labelFromSplitOnly[bLabel]) ?	ReadLength : 
+								MaxInsertSize;
+		size_t dist = (pos < aLabel.pos) ?  aLabel.pos - pos :
+						    pos - aLabel.pos;	
+		if(dist > bRange) continue;
+		//Check if the bLabel has already been assigned
+		if(!bestSetMap.count(bLabel)){
+		    bestSetMap[bLabel] = jRegLabelSet_t();
+		    bestSetMap[bLabel].insert(aLabel);
+		} else if(bestSetMap[bLabel].count(labelVec.at(first))){
+		    //bLabel has been assigned to a label with the same
+		    //score as aLabel
+		    bestSetMap[bLabel].insert(aLabel);
+		}
+	    }
+	    i++; 
+	} while(i < labelVec.size() && 
+		labelCount[labelVec[i]] == labelCount[labelVec[first]]);
+    }
+}
+
+
 //Parses candidate junctions and assigns each to the most prevalent region within the max
 //insert size; the range for paired reads is max insert size, the range for split reads is
 //read length
@@ -154,8 +222,8 @@ void OrderJunctions(const std::string fname, jRegLabelCount_t & labelCount,
 //	 - a reference to a regionMap to stor results in
 //	 - also used global max insert size and read length
 //Output - None, modifes the regionMap
-void ClusterRegions(const std::string fname, const jRegLabelCount_t & labelCount,
-		    const jRegLabelVector_t & labelVec, jRegMap_t & regionMap){
+void ClusterRegions(const std::string fname, const BestJRegSetMap_t & bestSetMap,
+		    jRegMap_t & regionMap) {
      fprintf(stderr,"Clustering Regions ...\n");
      std::ifstream in(fname);
      std::string chr, qname;
@@ -163,39 +231,24 @@ void ClusterRegions(const std::string fname, const jRegLabelCount_t & labelCount
      char score, strand;
      //Iterate over candidate junctions
      while (in >> chr >> off >> end >> qname >> score >> strand){
-	jRegLabel_t aLabel = {chr,strand,off};
-	bool bSplit = (qname[qname.length()-2] == '_');
-	int range = (bSplit) ? ReadLength : MaxInsertSize;
-	bool bFound = false;
-	//Search over labels from most to least prevalent
-	for (int i = 0; i < labelVec.size() && !bFound; i++){
-	    //start at i: test if in range
-	    //if i passes, then we can keep checking for ties
-	    //add the qname to every tied region
-	    int j = i;
-	    do {
-		const jRegLabel_t bLabel = labelVec[j];
-		if(aLabel.compare(bLabel,range) == 0){
-		    bFound = true;
-		    if(!regionMap.count(bLabel)){
-			regionMap[bLabel] = junctionRegion_t(bLabel.pos);
-		    }
-		    //Extend the bounds of the region
-		    if(off < regionMap[bLabel].left){
-			regionMap[bLabel].left = off;
-		    } else if(off > regionMap[bLabel].right){
-			regionMap[bLabel].right = off;
-		    }
-		    //Indicate if the region has split reads if necessary
-		    if(bSplit) regionMap[bLabel].nSplit++;
-		    regionMap[bLabel].QNameSet.insert(qname);
-		}
-		j++;
-	    } while(bFound && j < labelVec.size() &&
-	    	labelCount.at(labelVec[j]) == labelCount.at(labelVec[i]));
-	}
+	 jRegLabel_t aLabel = {chr,strand,off};
+	 bool bSplit = (qname[qname.length()-2] == '_');
+	 for( const auto & bLabel : bestSetMap.at(aLabel) ){
+	    if(!regionMap.count(bLabel)){
+		regionMap[bLabel] = junctionRegion_t(bLabel.pos);
+	    }
+	    //Extend the bounds of the region
+	    if(off < regionMap[bLabel].left){
+	        regionMap[bLabel].left = off;
+	    } else if(off > regionMap[bLabel].right){
+	        regionMap[bLabel].right = off;
+	    }
+	    //Indicate if the region has split reads if necessary
+	    if(bSplit) regionMap[bLabel].nSplit++;
+	    regionMap[bLabel].QNameSet.insert(qname);
+	 }
      }
-     fprintf(stderr,"Selected %lu unique regions\n",regionMap.size());
+     fprintf(stderr,"\nSelected %lu unique regions\n",regionMap.size());
 }
 
 //Outputs regions which have enough reads assigned,
